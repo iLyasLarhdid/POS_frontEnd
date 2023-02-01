ee2d979bbf2e34c569a1ef17e87b5d5bb7d54222
ilyas <larhdidok@gmail.com>
2023-02-01 17:32:25 +0000

    orders details page

--- src/views/components/viewOrders/ViewPage/Order.js
+++ src/views/components/viewOrders/ViewPage/Order.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { useCookies } from "react-cookie";
 import { useQuery } from "react-query";
 import config from "config";
-import { Card } from '@mui/material';
+import { Avatar, Card, Paper, styled, Table, TableBody, TableContainer, TableHead, TableRow } from '@mui/material';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
 import { Box } from '@mui/system';
 
 
@@ -19,7 +21,26 @@ const fetchData = async (key)=>{
     return res.json();
 }
 
-
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+        textAlign:'center'
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+        textAlign:'center'
+    },
+}));
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+'&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+},
+// hide last border
+'&:last-child td, &:last-child th': {
+    border: 0,
+},
+}));
 const Order = ({id})=>{
 
     //get the id from index page and retrieve data based on it
@@ -38,23 +59,50 @@ const Order = ({id})=>{
         autoComplete="off"
         noValidate
         >
-            hello {id}
-            <a href="https://github.com/iLyasLarhdid">my github</a>
-            <input type="email" formTarget0="_blank"/>
-            <table>
-                <tr>
-                    <td>id</td>
-                    <td>name</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </table>
+            <h1>Order N°{id}</h1>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                    <TableRow>
+                        <StyledTableCell>Img</StyledTableCell>
+                        <StyledTableCell>ID</StyledTableCell>
+                        <StyledTableCell align="right">name</StyledTableCell>
+                        <StyledTableCell align="right">price</StyledTableCell>
+                        <StyledTableCell align="right">Quantity</StyledTableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            data && data.products.map(row=>(
+                                <StyledTableRow key={row.id}>
+                                    <StyledTableCell component="th" scope="row">
+                                        <Avatar variant="square" src={`${config.host}/upload/viewFile/${row.product.pictures[0].title}`}/>
+                                    </StyledTableCell>
+                                    <StyledTableCell component="th" scope="row">
+                                        {row.id}
+                                    </StyledTableCell>
+                                    <StyledTableCell component="th" scope="row">
+                                        {row.product.name}
+                                    </StyledTableCell>
+                                    <StyledTableCell component="th" scope="row">
+                                        {row.price}
+                                    </StyledTableCell>
+                                    <StyledTableCell component="th" scope="row">
+                                        {row.quantity}
+                                    </StyledTableCell>
+                                </StyledTableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <h2 style={{display: 'flex',alignItems: 'center' }}><PointOfSaleIcon/> total : { data && data.products.map(row =>row.price * row.quantity).reduce((oldVal,newVal)=>oldVal+newVal) }</h2>
+
         </Box>
     </Card>
         
     </div>
     </div>)
-}   
+}
 
 export default Order;
